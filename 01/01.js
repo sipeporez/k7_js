7db91f0aa0fb820ccd89d564f5b0751a15c02525
sipeporez <sipeporez@gmail.com>
2024-04-23 17:54:54 +0900

    0423

--- 01/01.js
+++ 01/01.js
@@ -1,1 +1,12 @@
-alert('안녕하세요.'); 
+function handleClick(n) {
+    // document.querySelector('#msg').innerHTML = '<h1>안녕하세요!</h1>';
+    document.querySelector('#msg').innerHTML = `<h1>안녕하세요! ${n}</h1>`;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // alert('안녕하세요');
+    // document.getElementById('msg').innerHTML = '안녕하세요';
+    //document.querySelector('#msg').innerHTML = '<h1>안녕하세요</h1>';
+
+});
