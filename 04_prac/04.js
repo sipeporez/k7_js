5f40f612c5ef7a50d7690e41d8907325430539f9
sipeporez <sipeporez@gmail.com>
2024-04-25 16:14:57 +0900

    js code refactoring used loops

--- 04_prac/04.js
+++ 04_prac/04.js
@@ -1,110 +1,73 @@
-document.addEventListener ('DOMContentLoaded', () => {
+/*
+    1. DOM 에서 제어할 노드(속성)를 가져오기
+    2. 6개의 버튼 클릭 이벤트 확인하기
+    3. 버튼 클릭이 되면
+        - 해당하는 버튼의 숫자를 추출 => 사용자 선택 수(user)  => user 이미지 변경
+        - 랜덤 숫자를 생성 => 컴퓨터 선택 수 (com) => com 이미지 변경
+        - user와 com의 수 비교
+        - 결과 출력
+*/
 
-    const btDiv = document.getElementById('btDiv2');
+document.addEventListener('DOMContentLoaded', () => {
+    /*
+        querySelector 선택자
+        태그    : '태그'
+        클래스  : '.클래스'
+        ID      : '#ID'
+        복합    : '태그 태그'
+    */
+
+    const IMG_COM = document.querySelector('.img_com');
+    const IMG_USER = document.querySelector('.img_user');
+    const msg = document.querySelector('.is_correct h1')
+
+    //selectorAll => 속성에 해당하는 모든 값을 노드리스트 형식으로 가져옴
+    const bt = document.querySelectorAll('section button');
+
+    // 1. for 반복문 
+    // for (let i = 0 ; i < bt.length ; i++) {
+    //     console.log(bt[i]);
+    // }
+
+    // 2. for in 반복문 - 뒤에 여러 속성이 붙어옴
+    // for (let i in bt) {
+    //     console.log(bt[i]);
+    // }
+
+    // 3. forEach 반복문
+    // bt.forEach (bt => {
+    //     console.log(bt);
+    // })
+
+    // 4. for of 반복문
+    // for (let b of bt) {
+    //     console.log(b);
+    // }
+    // 4-1. for of 반복문 (entry)
+    // for (let [i,b] of bt.entries()) {
+    //     console.log(i,bt);
+    // }
+
+    // forEach 반복문을 통해 가져온 모든 버튼에 클릭이벤트 생성, i는 반복 횟수
+    bt.forEach(i => {
+        i.addEventListener('click', () => {
+
+            // 랜덤 난수 생성 (범위 1~6)
+            let rand = Math.floor(Math.random() * 6) + 1;
+            console.log(i.innerHTML.substring(0,1));
+            // innerHTML로 각 버튼에 해당하는 속성값 출력
+            // substring()으로 문자열을 잘라내어 사용자 숫자 추출
+            // 백틱 문자열 `` 로 이미지 경로에 숫자 삽입하여 이미지 변경
+            IMG_COM.setAttribute('src', `../img/01_dice/${rand}.png`);
+            IMG_USER.setAttribute('src', `../img/01_dice/${i.innerHTML.substring(0, 1)}.png`);
+
+
+            // 랜덤값과 사용자 숫자값에 따른 조건문
+            if (rand == i.innerHTML.substring(0, 1))
+                msg.innerHTML = '맞췄습니다';
+            else
+                msg.innerHTML = '틀렸습니다';
+        });
+    })
     
-    const bt1 = document.createElement('button');
-    const bt2 = document.createElement('button');
-    const bt3 = document.createElement('button');
-    const bt4 = document.createElement('button');
-    const bt5 = document.createElement('button');
-    const bt6 = document.createElement('button');
-
-    const bt1Txt = document.createTextNode('1번');
-    const bt2Txt = document.createTextNode('2번');
-    const bt3Txt = document.createTextNode('3번');
-    const bt4Txt = document.createTextNode('4번');
-    const bt5Txt = document.createTextNode('5번');
-    const bt6Txt = document.createTextNode('6번');
-    
-    const IMG = document.querySelector('.img_com');
-    const IMG2 = document.querySelector('.img_user');
-    const check = document.querySelector('.is_correct');
-
-
-    const btCreate = (bt, idTxt, captionNode, parentNode) => {
-        bt.setAttribute('id', idTxt);
-        bt.appendChild(captionNode);
-        parentNode.append(bt);
-    }
-
-    btCreate(bt1, 'bt1', bt1Txt, btDiv);
-    btCreate(bt2, 'bt2', bt2Txt, btDiv);
-    btCreate(bt3, 'bt3', bt3Txt, btDiv);
-    btCreate(bt4, 'bt4', bt4Txt, btDiv);
-    btCreate(bt5, 'bt5', bt5Txt, btDiv);
-    btCreate(bt6, 'bt6', bt6Txt, btDiv);
-
-    bt1.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-        
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/1.png`);
-                
-        if (rand == 1)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-
-    bt2.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/2.png`);
-                
-        if (rand == 2)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-
-    bt3.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/3.png`);
-                
-        if (rand == 3)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-
-    bt4.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/4.png`);
-                
-        if (rand == 4)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-
-    bt5.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/5.png`);
-                
-        if (rand == 5)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-
-    bt6.addEventListener('click',()=>{
-        rand = Math.floor(Math.random()*6)+1;
-
-        IMG.setAttribute('src', `../img/01_dice/${rand}.png`);
-        IMG2.setAttribute('src', `../img/01_dice/6.png`);
-        
-        if (rand == 6)
-            check.innerHTML = ('<h1>맞췄습니다.</h1>');
-        else 
-            check.innerHTML = ('<h1>틀렸습니다.</h1>');
-    });
-    
-
 });
