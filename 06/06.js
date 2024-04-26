e738d5bec23a671ad06b742e1dd718036e2c8855
sipeporez <sipeporez@gmail.com>
2024-04-26 17:24:45 +0900

    changed css

---
+++ 06/06.js
@@ -0,0 +1,15 @@
+/* 
+    1. 값이 입력되면
+        - 1. select box 선택값 읽기 (option value)
+        - 2. input box 값 읽기
+        - 3. 실시간으로 값을 변환하여 오른쪽 input box에 넣기
+    2. 오른쪽 input box는 입력 못하게 막기
+
+    3. 가능하면 select box 2개 연동하여 단위를 바꿀때 같이 바뀌게 하기
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+
+})
