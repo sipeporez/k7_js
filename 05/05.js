7d3ff79864ba400cd38059312b0b053c02e7f88e
sipeporez <sipeporez@gmail.com>
2024-04-26 16:10:48 +0900

    added count

--- 05/05.js
+++ 05/05.js
@@ -15,52 +15,82 @@ document.addEventListener('DOMContentLoaded', () => {
     const IMG = document.querySelector(".updown_img");
     const bt = document.querySelector('.button');
     const input = document.querySelector('input');
-    
+    const ct = document.querySelector('.count');
+
     // flag 값을 사용한 랜덤 수 고정
     let rand;
     let flag = true;
-    
+    let count = 10;
+
     bt.addEventListener('click', (e) => {
+
         // form 태그 사용시 콜백되지 않도록
         e.preventDefault();
 
-        let val = input.value;
+        // 인풋박스 입력값 받기
+        let inval = input.value;
 
-        // flag가 true일때만 랜덤 수 생성
+        // flag가 true일때 초기화
         if (flag) {
             flag = false;
-            rand = Math.floor(Math.random()*100)+1;
+            rand = Math.floor(Math.random() * 100) + 1;
+
+            count = 10;
+            ct.innerHTML = `<h3>남은 횟수 : ${count}회</h3>`;
+
+            IMG_NAME = "what";
+            IMG.setAttribute('src', `../img/01_updown/${IMG_NAME}.png`);
+            
+            input.style.display = 'flex';
+            bt.textContent = ('확인');
         }
 
+        // 확인버튼 누를때마다 인풋박스 초기화+하이라이트
+        input.focus();
+        input.value = '';
+
+        if (rand < inval || rand > inval)
+            count--;
+
+        if (count == 0) {
+            alert("실패하셨습니다. 숫자를 다시 생성 해주세요.");
+            input.style.display = 'none';
+            bt.textContent = ('숫자를 다시 생성 해주세요');
+            flag = true;
+        }
+        console.log(count)
         // input값 확인
         // 같은경우
-        if (rand == val) {
-            IMG.setAttribute('src','../img/01_updown/good.png');
-            input.remove();
-            bt.innerHTML='숫자를 생성 해주세요';
-            bt.addEventListener('click',() => {
-                location.reload();
-            });
+        if (rand == inval) {
+            IMG_NAME = "good";
+            IMG.setAttribute('src', `../img/01_updown/${IMG_NAME}.png`);
+            input.style.display = 'none';
+            alert(`${10-count+1}회 만에 성공하셨습니다.\n숫자를 다시 생성 해주세요.`)
+            bt.textContent = ('숫자를 다시 생성 해주세요');
+            count = 10;
+            flag = true;
         }
         // 입력되지 않은 경우
-        else if (val == '')  {
-            alert('숫자를 입력해주세요');
-            input.focus();
-            return ;
+        else if (inval == '') {
+            alert('숫자를 입력해주세요' + rand);
+            count++;
+            return;
         }
         // 범위를 벗어난 경우
-        else if (val < 1 || val > 100 ) {
+        else if (inval < 1 || inval > 100) {
             alert('1~100까지의 숫자를 입력해주세요');
-            input.focus();
-            return ;
+            count++;
+            return;
         }
         // 작은 경우 - up 이미지
-        else if (rand > val)
-            IMG.setAttribute('src','../img/01_updown/up.png');
-
+        else if (rand > inval)
+            IMG.setAttribute('src', '../img/01_updown/up.png');
         // 큰 경우 - down 이미지
-        else if (rand < val)
-            IMG.setAttribute('src','../img/01_updown/down.png');
-    })
-    
+        else if (rand < inval)
+            IMG.setAttribute('src', '../img/01_updown/down.png');
+
+        // 클릭 이후 바뀌는 부분은 맨 밑으로
+        ct.innerHTML = `<h3>남은 횟수 : ${count}회</h3>`;
+    }
+    )
 });
